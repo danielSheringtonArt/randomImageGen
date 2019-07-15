9d40209f5dc1cf2c3b4aeea062a49a6130224dbd
danielSheringtonArt <52900864+danielSheringtonArt@users.noreply.github.com>
2019-07-15 12:33:03 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// placed images in this array
+var random_images_array = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.jpg', 'img8.jpg', 'img9.jpg', 'img10.jpg', 'img11.jpg'];
+    
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
